93296f8e5f09a065713e95cf2cf1a3d79130519a
Joseph Kempster <jkempster34@gmail.com>
2021-04-19 16:46:27 +0100

    Migration to add TeachingEvent CrmName

---
+++ GetIntoTeachingApi/Migrations/20210419153743_AddCrmNameToTeachingEvent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GetIntoTeachingApi.Migrations
+{
+    public partial class AddCrmNameToTeachingEvent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CrmName",
+                table: "TeachingEvents",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CrmName",
+                table: "TeachingEvents");
+        }
+    }
+}
